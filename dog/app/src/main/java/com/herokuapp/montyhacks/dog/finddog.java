76320c57b83aea21a6fbbeb56cb656b9793fbb6d
Julia Guo <Julia Guo>
2016-11-19 17:14:58 -0500

    not sydney

--- dog/app/src/main/java/com/herokuapp/montyhacks/dog/finddog.java
+++ dog/app/src/main/java/com/herokuapp/montyhacks/dog/finddog.java
@@ -39,8 +39,8 @@ public class finddog extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng mylocation = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(mylocation).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mylocation));
     }
 }
